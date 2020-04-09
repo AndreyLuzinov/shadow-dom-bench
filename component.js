e3834f3ca5a96957dea0dcd7b6862c29192dc52f
Andrey Luzinov <aluzinov@accesssoftek.com>
2020-04-09 14:05:44 +0300

    initial

---
+++ component.js
@@ -0,0 +1,17 @@
+const currentDocument = document.currentScript.ownerDocument;
+
+class Component extends HTMLElement {
+  constructor() {
+    // If you define a constructor, always call super() first as it is required by the CE spec.
+    super();
+  }
+  
+  connectedCallback() {
+	  const shadowRoot = this.attachShadow({mode: 'open'});
+	  const root = currentDocument.createElement("button");
+	  root.innerText = "Button";
+	  shadowRoot.appendChild(root);
+  }
+}
+
+customElements.define('my-component', Component);
